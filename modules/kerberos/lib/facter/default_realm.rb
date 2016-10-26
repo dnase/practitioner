e4ac49b6b938ea2023098439c39541b5d3f4e0c0
mcdrewber <mcdrewber@puppetlabs.vm>
2016-10-26 19:38:55 +0000

    fixed ruby fact

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,8 +1,11 @@
 Facter.add('default_realm') do
-  #setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
-  File.read('/etc/krb5.conf').each_line do |line|
-    if line.include? "default_realm"
-      setcode line.match(/.*\=\s(\w+\.\w{3})/)[1]
+  setcode do
+    realm = nil
+    File.read('/etc/krb5.conf').each_line do |line|
+      if line.include? "default_realm"
+        realm = line.match(/.*\=\s(\w+\.\w{3})/)[1]
+      end
     end
+    realm
   end
 end
