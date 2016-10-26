b0de1749d61eb768c04e1d9f1f5904e5a5c0a40f
mcdrewber <mcdrewber@puppetlabs.vm>
2016-10-26 19:29:07 +0000

    refactored custom fact into ruby

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,3 +1,8 @@
 Facter.add('default_realm') do
-  setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+  #setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+  File.read('/etc/krb5.conf').each_line do |line|
+    if line.include? "default_realm"
+      setcode line.match(/.*\=\s(\w+\.\w{3})/)[1]
+    end
+  end
 end
